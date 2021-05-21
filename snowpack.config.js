c982a96cf81a5b5d1c5a6a2b2830dd0ff8a60fe0
ridho muhammad <mridho@ruangguru.com>
2021-05-21 10:49:09 +0800

    chore(setup): add svelte into project

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,10 +1,11 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+   public: '/',
+   src: '/dist',
   },
   plugins: [
-    /* ... */
+    '@snowpack/plugin-svelte'
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
