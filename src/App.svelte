030cb2fb935ec7f4b698d695ba7ca84dc3b434ed
ridho muhammad <mridho@ruangguru.com>
2021-05-23 12:13:19 +0800

    feat(setup): add wired elements

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,11 @@
 <script>
-  /* component logic will go here */
+   import "wired-elements";
 </script>
-<style>
-  /* css will go here */
-</style>
-<div class="App">
-  <header class="App-header">
-    <h1>Welcome</h1>
-    <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-      Learn Svelte
-    </a>
-  </header>
+<div id="App">
+<wired-card elevation="5">
+  <h1>
+    wired-elements demo
+  </h1>
+</wired-card>
+    <h1>You did it!</h1>
 </div>
